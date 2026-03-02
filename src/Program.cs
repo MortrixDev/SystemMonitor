32578ae3ab080217ad76fea998911ca9f1902863
Mortrix <sebastiansommerdk@gmail.com>
2026-03-03 00:25:00 +0100

    Moved wwwroot/ and fixed Program.cs to serve static files correctly

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<SystemMonitor.SystemMonitor>();
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(policy => {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseCors();
 app.MapGet("/stats", async (SystemMonitor.SystemMonitor monitor)  => {
 	var stats = await monitor.GetSystemStats();
     return Results.Ok(stats);
